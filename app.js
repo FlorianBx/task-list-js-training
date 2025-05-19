5cf76f5b509d8517a2ec05b2ba360d606754648e
Florian Beaumont <25250981+BFlorian91@users.noreply.github.com>
2025-05-19 23:01:14 +0200

    init

---
+++ app.js
@@ -0,0 +1,70 @@
+// TODO: 1 - Récupérer les éléments HTML à manipuler
+//    - L'input pour la tâche (#task-input)
+//    - Le formulaire (#task-form)
+//    - La liste UL (#task-list)
+//    - Le bouton pour tout effacer (#clear-tasks)
+
+
+
+
+//-------------------------------------------------------------
+
+// TODO: 2 - Créer un tableau JS pour stocker les tâches
+//    - Soit un tableau de strings ("xyz"), soit (PLUS PROPRE) un tableau d'objets [{ text, done }, ...]
+
+
+
+
+//-------------------------------------------------------------
+
+// TODO: 3 - (Bonus) Charger les tâches sauvegardées dans le localStorage au chargement, sinon tableau vide
+
+
+
+
+
+//-------------------------------------------------------------
+
+// TODO: 4 - Fonction pour afficher toutes les tâches dans le HTML
+//    - Utilise une boucle pour générer les <li> avec texte + bouton supprimer + classe "done" si besoin
+
+
+
+
+
+//-------------------------------------------------------------
+
+// TODO: 5 - Gestion de l'ajout d'une tâche
+//    - À la soumission du form : preventDefault(), vérifie input non vide (trim())
+//    - Ajoute dans le tableau, reset l'input, met à jour affichage et sauvegarde
+
+
+
+
+
+//-------------------------------------------------------------
+
+// TODO: 6 - Suppression d'une tâche
+//    - Bouton delete sur chaque tâche -> retire du tableau -> rafraîchit affichage et sauvegarde
+
+
+
+
+
+//-------------------------------------------------------------
+
+// TODO: 7 - Marquer une tâche comme faite
+//    - Clic sur le texte : toggle la propriété "done", bonus press Entrer pour ajouter la classe "done", sauvegarde
+
+
+
+
+
+//-------------------------------------------------------------
+
+// TODO: 8 - "Tout effacer" : vide le tableau et localStorage, rafraîchis
+
+
+
+
+
